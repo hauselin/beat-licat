dfb0bb2c39a3422360e53084c48975e417f3b60b
hauselin <hauselin@gmail.com>
2020-09-24 23:13:06 -0400

    simplify code

--- beat_licat.py
+++ beat_licat.py
@@ -17,19 +17,20 @@ payload = {
     "DNT": 1,
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:81.0) Gecko/20100101 Firefox/81.0",
 }
+current_ver = "v0.9.1"
 
 
-def notify(title, text, url):
+def notify(title, text):
     os.system(f"""osascript -e 'display notification "{text}" with title "{title}"'""")
-    webbrowser.open(url, new=1)
 
 
-def check_page(url):
+def check_version(url):
     page = requests.get(url)
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, "html.parser")
         new = soup.find_all("a", class_="muted-link css-truncate")[0]
-    return new
+        ver = new["title"]
+    return ver
 
 
 def write_to_clipboard(output):
@@ -39,29 +40,27 @@ def write_to_clipboard(output):
     process.communicate(output.encode("utf-8"))
 
 
-def compare_old_new(old, new):
-    if old != new:  # if new release
+def compare_old_new(old_ver, new_ver):
+    if old_ver != new_ver:  # if new release
         webbrowser.open(discord_url, new=1)
-        notify("New Obsidian release!", "Quick! BEAT LICAT!", github_url)
-        v = new["title"]
+        notify("New Obsidian release!", "Quick! BEAT LICAT!")
+        webbrowser.open(github_url, new=1)
         # copy text to clipboard
-        spoiler_txt = f"SPOILER ALERT: @Licat will release Obsidian {v} in a few seconds... ausius-Licat scoreboard: 3-2 \U0001F923\U0001F923"
+        spoiler_txt = f"SPOILER ALERT: @Licat will release Obsidian {new_ver} in a few seconds... ausius-Licat scoreboard: 3-2 \U0001F923\U0001F923"
         write_to_clipboard(spoiler_txt)
-        return new, False  # break loop
+        return False  # break loop
     else:
         print("No new release yet.")
-        return new, True  # continue loop
+        return True  # continue loop
 
 
 def start_checking():
-    old = check_page(github_url)
-    i = 0
     status = True
     while status:
-        i += 1
-        print(f"Checking for Obisidian release (scrape {i})...")
-        new, status = compare_old_new(old, check_page(github_url))
+        print("Checking for Obisidian release...")
+        status = compare_old_new(current_ver, check_version(github_url))
         if status:
+            print("Continuing...")
             time.sleep(random.randint(3, 10))
 
 
